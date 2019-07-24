6cb9eb029e936bd65eaf2c52402f1904a965eafe
Paulo B. Carvalho <paulo.bonfleur@gmail.com>
2019-07-24 10:24:37 -0300

    Ok

--- gestao_produtos/settings.py
+++ gestao_produtos/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['app-gestao-produtos.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['app-gestao-produtos.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
